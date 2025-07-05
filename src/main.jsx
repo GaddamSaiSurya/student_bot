8922d61b427b5e1a8e20dadbc6d576a04b1219b1
GaddamSaiSurya <134715460+GaddamSaiSurya@users.noreply.github.com>
2025-07-05 11:42:11 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CollegeGPTUI from './CollegeGPTUI.jsx';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CollegeGPTUI />
+  </React.StrictMode>
+);
